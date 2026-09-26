5c44e9ff9e00277178bff013840977c18f036b1d
logoraz <erikalmaraz@fastmail.com>
2026-09-26 14:02:40 -0500

    Chore: clean-up

--- contrib/scratch.lisp
+++
@@ -1,9 +0,0 @@
-(defpackage #:scratch
-  (:use #:cl #:lem)
-  (:import-from #:lem-core)
-  (:local-nicknames (#:u #:uiop)
-                    (#:alx #:alexandria))
-  (:documentation "scratch"))
-
-(in-package #:scratch)
-
